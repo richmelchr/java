5db6ec6e8a1c824d87738ef9b5348acf5a7ecfdf
Richard Melcher <richmelchr@gmail.com>
2016-03-07 03:08:51 -0700

    java advanced notes outline

---
+++ src/advanced.java
@@ -0,0 +1,58 @@
+public class advanced {
+
+}
+
+
+
+
+
+
+
+/*
+02 Java 7 New Features
+    An overview of Java 7 new features
+    Exploring the simplified use of generics
+    Using underscores in numeric literals
+    Using strings in switch statements
+03 Using Advanced Class Structures
+    Using static initializer
+    Using instance field initializer
+    Using member classes
+    Using local inner classes
+    Using anonymous inner classes
+    Creating and using enumeration classes
+04 Using the Reflection API
+    Using the Class class
+    Instantiating classes dynamically
+    Navigating inheritance trees
+05 More of the Collections Framework
+    Managing orders sets with TreeSet
+    Controlling list order with LinkedList
+    Navigating inheritance trees
+    Peeking and polling with queues
+    hashset
+06 Testing and Advanced Exception Handling
+    Exploring test-driven development with the assert keyword
+    Using the finally keyword
+    Using try-with-resources in Java 7
+    Defining and throwing a custom exception
+07 Managing Files and Directories in Java 7
+    Using the Path class
+    Managing files and directories
+    Reading and writing text files
+    Walking the directory tree
+    Finding files
+    Watching a directory for file changes
+08 Working with IO Streams
+    Reading and writing byte streams one
+    Reading and writing byte streams two
+    Using buffered streams
+    Scanning tokenized text
+09 Working with Multi-threading
+    Extending the Thread class
+    Implementing the Runnable interface
+    Interrupting a thread
+    Synchronizing threads
+10 Conclusion
+    Where to go from here
+*/
